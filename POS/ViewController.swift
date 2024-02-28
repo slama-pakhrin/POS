8847a33435fce9ffdb29be1a427e684c687894e2
slama-pakhrin <salonlama47@gmail.com>
2024-02-27 19:00:50 -0500

    Initial Commit

---
+++ POS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POS
+//
+//  Created by Arii on 2024-02-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
