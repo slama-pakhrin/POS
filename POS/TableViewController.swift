9ba71abec967d429dbdc1889851e593c97474d0e
slama-pakhrin <salonlama47@gmail.com>
2024-03-05 02:10:43 -0500

    Edit Data Update

--- POS/TableViewController.swift
+++ POS/TableViewController.swift
@@ -9,21 +9,28 @@ import UIKit
 
 class edit: UITableViewController {
 
+   
     override func viewDidLoad() {
         super.viewDidLoad()
       
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return dataCollection.data.count;
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 0
+        return dataCollection.data.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "editCell", for: indexPath) as! editTableViewCell
+        cell.labelAtTop.text = "\(dataCollection.data[indexPath.row].title) (\(dataCollection.data[indexPath.row].quantity))"
+        cell.labelAtBottom.text = "$ \(String(dataCollection.data[indexPath.row].price))"
+        return cell
+    }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "anythingWorks", bundle: nil)
+        let myViewController = storyboard.instantiateViewController(withIdentifier: "changeDataVC")
+        present(myViewController, animated: true)
+    }
 //    @IBAction func doneClicked(_ sender: Any) {
 //        performSegue(withIdentifier: "unwind", sender: self)
 //    }
---
+++ POS/editTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  editTableViewCell.swift
+//  POS
+//
+//  Created by Arii on 2024-03-05.
+//
+
+import UIKit
+
+class editTableViewCell: UITableViewCell {
+    @IBOutlet weak var labelAtTop: UILabel!
+    
+    @IBOutlet weak var labelAtBottom: UILabel!
+}
